d3907bfe848b810496b80ea6adf787c8b534193d
haja <haja@student.chalmers.se>
2025-09-17 21:01:59 +0200

    Setup PostgreSQL connection in db.ts

---
+++ backend/src/db.ts
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.DB_USER || 'tinyshop_user',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'tiny_webshop',
+  password: process.env.DB_PASS || 'password',
+  port: Number(process.env.DB_PORT) || 5432,
+});
+
+export default pool;
