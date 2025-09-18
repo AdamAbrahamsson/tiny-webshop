f21b409b69cb853df92c808b0a55a0bacb7b9231
haja <haja@student.chalmers.se>
2025-09-18 09:18:09 +0200

    Modify the index.ts to include the routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,18 @@
 import express from 'express';
+import productRoutes from './routes/products';
+import authRoutes from './routes/auth';
+import orderRoutes from './routes/orders';
 
 const app = express();
 const PORT = 3000;
+
+// Parsing the incoming request bodies that are in JSON Format 
+app.use(express.json());
+
+// Routes
+app.use('/api/products', productRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello Tiny Webshop');
