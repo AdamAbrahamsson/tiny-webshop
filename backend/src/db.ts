367005316d042204044608e0c5888a041d768901
haja <haja@student.chalmers.se>
2025-09-18 12:07:03 +0200

    Add the CRUD operations for the products in the products controller and router and update the DB host

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   user: process.env.DB_USER || 'tinyshop_user',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'db',
   database: process.env.DB_NAME || 'tiny_webshop',
   password: process.env.DB_PASS || 'password',
   port: Number(process.env.DB_PORT) || 5432,
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import productRoutes from './routes/products';
-import authRoutes from './routes/auth';
-import orderRoutes from './routes/orders';
+//import authRoutes from './routes/auth';
+//import orderRoutes from './routes/orders';
 
 const app = express();
 const PORT = 3000;
@@ -11,8 +11,8 @@ app.use(express.json());
 
 // Routes
 app.use('/api/products', productRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/orders', orderRoutes);
+//app.use('/api/auth', authRoutes);
+//app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello Tiny Webshop');
